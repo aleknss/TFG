d20d9667d4e756f2e48002e86ad52af3505f94cd
alek <damj12.2024@gmail.com>
2025-01-09 05:51:21 +0100

    Declaración de estructura backend y frontend. Frontend: mejora de Home

---
+++ frontend/tfg/src/components/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import caja from "../assets/images/caja.png";
+
+function Home() {
+  return (
+    <div>
+      <div className="apartado1 flex h-[600px] items-center mx-14">
+        <div className="w-3/5 justify-center flex-col flex gap-4">
+          <h1>
+            Control Total de tu <br /> Inventario en Tiempo Real
+          </h1>
+          <p>
+            Optimiza la gestión de tus productos, reduce costes y mejora la
+            eficiencia de tus entregas con nuestra solución integral de gestión
+            de inventario
+          </p>
+          <p className="boton">PRUÉBALO</p>
+        </div>
+        <div className="w-2/5 justify-center items-center flex">
+          <img src={caja} alt="Caja" className="w-1/2" />
+        </div>
+      </div>
+
+      <div className="apartado2 flex h-[600px] items-center">
+        <div>
+          <h1>¿A quién vamos dirigidos?</h1>
+          <ul>
+            <li>
+              <p>Comercio</p>
+            </li>
+            <li>
+              <p>Industria</p>
+            </li>
+            <li>
+              <p>Bodegas</p>
+            </li>
+            <li>
+              <p>Distribuidores</p>
+            </li>
+            <li>
+              <p>Restaurantes</p>
+            </li>
+            <li>
+              <p>Supermercados</p>
+            </li>
+            <li>
+              <p>Casas</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/tfg/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#4725E1",
+      },
+    },
+  },
+  plugins: [],
+};
